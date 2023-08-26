842e4d055fef6a901ba09a9dd47f476b4af260e5
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2023-08-26 16:35:49 +0100

    fixed index nullReferenceException error

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -18,15 +18,10 @@ public class IndexModel : PageModel
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
-        requestKey = Guid.NewGuid().ToString();
-        apiKey = "8da556fa346f73544d38961d93d64774ef62a5a235a9db8800e6138110184efef5144b015f184394e179644da37218e6d455d6627feb1bec66e1ba1d0dc1fade";
-        apiSecret = "a24c87f6d0560a8910bd6d6848218e0c9eca2aa9143642108c74cc76bc6ee848dbac80a1855844ca3d64fbafa60a8cd204d8322df8a2426ddb9eab2314b20c94";
 
-        string enc = Sha512Hash(requestKey+apiKey+apiSecret);
-        ViewData["Authorisation"] = enc;
     }
 
-    public static string Sha512Hash(string data)
+    public StringBuilder Sha512Hash(string data)
 {
     var message = Encoding.UTF8.GetBytes(data);
     var hex = new StringBuilder();
@@ -38,11 +33,22 @@ public class IndexModel : PageModel
         {
             hex.Append(string.Format("{0:x2}", x));
         }
-        return hex.ToString();
+        return hex;
     }
 }
     public void OnGet()
     {
+        requestKey = Guid.NewGuid().ToString();
+        apiKey = "8da556fa346f73544d38961d93d64774ef62a5a235a9db8800e6138110184efef5144b015f184394e179644da37218e6d455d6627feb1bec66e1ba1d0dc1fade";
+        apiSecret = "a24c87f6d0560a8910bd6d6848218e0c9eca2aa9143642108c74cc76bc6ee848dbac80a1855844ca3d64fbafa60a8cd204d8322df8a2426ddb9eab2314b20c94";
 
+        var toenc = new StringBuilder();
+        toenc.Append(requestKey);
+        toenc.Append(apiKey);
+        toenc.Append(apiSecret);
+
+        var enc = this.Sha512Hash(toenc.ToString());
+        
+        ViewData["Authorisation"] = enc.ToString();
     }
 }
